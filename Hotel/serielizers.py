eb5dcb236ff9fc2c12df7352d3650f7adf6e215f
root <>
2023-02-19 12:28:09 +0300

    updated serielizers

--- Hotel/serielizers.py
+++ Hotel/serielizers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
-from .models import Eatery, Branch
+from .models import Eatery, Branch, FoodCategory, FoodSubCategory, FoodProduct
 
 class BranchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Branch
+        fields = "__all__"
+        depth =3
+
+
+class FoodCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FoodCategory
+        fields = "__all__"
+        depth =3
+
+
+class FoodSUBCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FoodSubCategory
+        fields = "__all__"
+        depth =3
+
+class FoodProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FoodProduct
         fields = "__all__"
         depth =3
