6abb89888e23c1803cf9e40f2af679513bb0abef
root <>
2023-02-19 12:27:49 +0300

    updated hotel urls

--- Hotel/urls.py
+++ Hotel/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('eterly/verify/', views.eterly_verification, name='eterly_verification'),
     path('branch/create/', views.create_branch, name='create_branch'),
     path('branch/', views.get_all_my_branches, name='get_all_my_branches'),
+    path('addcat/', views.add_food_category, name='add_food_category'),
+    path('addsubcat/', views.add_food_sub_category, name='add_food_sub_category'),
+    path('add/', views.add_food_product, name='add_food_sub_category'),
 
 
     path('order-product/', views.orderProduct, name='order_product'),
