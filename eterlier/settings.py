55f61708ec2c02b3649b43f0eafa8f23db34f795
Stephen-Kamau <stiveckamash@gmail.com>
2023-05-02 22:51:25 +0300

    updated the django pil

--- eterlier/settings.py
+++ eterlier/settings.py
@@ -25,16 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "sbkjsdlsilknwsdkwl2849y32gflfmbvdghbjkslfgnhfdklsfdjhkfdls"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS =  ["*"]#config("ALLOWED_HOSTS" , cast = Csv())
 
 # SMS TOKENS INFOR
-SMS_SID = config('SMS_SID')
-SMS_TOKEN = config('SMS_TOKEN')
+SMS_SID = "dhuisbk"#config('SMS_SID')
+SMS_TOKEN = "sihvdbklsndj"#config('SMS_TOKEN')
 
 
 # Application definition
